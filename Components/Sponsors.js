130d3f4932428dc0b4c79e13cabf5b756d5f022e
Maxjansson <44931@gymnasium.ax>
2022-04-07 08:26:15 +0300

    css

--- Components/Sponsors.js
+++ Components/Sponsors.js
@@ -8,7 +8,7 @@ function Sponsors({sponsors}){
             {sponsors.map(({logoUrl,sponsorUrl,name}, i) => ( //maps out sponsors
                 <div key={i}>
                     
-                    <a passHref={true} href={sponsorUrl} className="sponsorImage" dangerouslySetInnerHTML={{__html: logoUrl}}/>
+                    <a passHref={true} href={sponsorUrl} className="sponsorHomepage" dangerouslySetInnerHTML={{__html: `<img src="${logoUrl}" alt="sponsor image" class="sponsorImage"/>`}}/>
                     <p className="sponsorInfo">{name}</p>
                     <br/>
                 </div>
