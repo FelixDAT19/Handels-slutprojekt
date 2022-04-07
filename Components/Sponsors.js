2aa787b032183f8db0971e65682eaa8c06ce5643
FelixDAT19 <61045804+FelixDAT19@users.noreply.github.com>
2022-04-07 08:36:28 +0300

    sponsors

--- Components/Sponsors.js
+++ Components/Sponsors.js
@@ -7,11 +7,7 @@ function Sponsors({sponsors}){
             {sponsors.map(({logoUrl,sponsorUrl,name}, i) => ( //maps out sponsors
                 <div key={i}>
                     
-<<<<<<< HEAD
                     <Link href={sponsorUrl} className="sponsorHomepage" passHref><img src={logoUrl} alt="sponsorimgae" className="sponsorImage"></img></Link>
-=======
-                    <a passHref={true} href={sponsorUrl} className="sponsorHomepage" dangerouslySetInnerHTML={{__html: `<img src="${logoUrl}" alt="sponsor image" class="sponsorImage"/>`}}/>
->>>>>>> 1132ebedc7e320d4e29c213559f7866af4276cb1
                     <p className="sponsorInfo">{name}</p>
                     <br/>
                 </div>
