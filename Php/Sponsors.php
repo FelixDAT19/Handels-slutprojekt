da28cf34c0dbb29392967c64591143c2e4e39e98
Wegenfelt <100673265+Wegenfelt@users.noreply.github.com>
2022-04-20 09:58:36 +0300

    Update Sponsors.php

--- Php/Sponsors.php
+++ Php/Sponsors.php
@@ -65,7 +65,7 @@ if (isset($_POST['editSponsor']) && $_POST['editSponsor'] != "") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dropdown.css">
+    <link rel="stylesheet" href="adminpage.css">
     <title>Sponsors</title>
 </head>
 
