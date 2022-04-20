276ad13f318e7abfb97969815f64f1e903667436
Wegenfelt <100673265+Wegenfelt@users.noreply.github.com>
2022-04-20 09:57:42 +0300

    Update AdminPage.php

--- Php/AdminPage.php
+++ Php/AdminPage.php
@@ -60,7 +60,7 @@ if (isset($_POST['deleteOpenHours'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dropdown.css">
+    <link rel="stylesheet" href="adminpage.css">
     <title>Admin main</title>
 </head>
 
