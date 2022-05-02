c886fbb53ae46ffa5902f7b11b67a72beeccd4aa
Wegenfelt <wegenfelt@gmail.com>
2022-05-02 10:45:59 +0300

    divblock

--- Php/AdminPage.php
+++ Php/AdminPage.php
@@ -113,85 +113,85 @@ if (isset($_POST['deleteQrData'])) {
 
         ?>
         <div class="contentbox">
-        <div class="tablebox">
-        <table>
-            <thead>
-                <tr>
-                    <th>Tävlingsägare:</th>
-                    <th>Länk till tävling:</th>
-                    <th>Ta bort:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                competitionList($db);
-                ?>
-            </tbody>
-        </table>
-    </div>
-        <div class="inputbox">
-            <form method="POST">
-                <select name="companies" class="selectCompany">
-                    <?php
-                    selectCompany($db);
-                    ?>
-                </select><br>
-                <input type="url" id="formUrl" name="formUrl" placeholder="Länk till ny tävling" maxlength="500" autocomplete="off"><br>
-                <button name="createCompetition" type="submit">Lägg till</button>
-            </form>
-        </div>
-    </div>
-
-    <div class="contentbox">
-        <div class="tablebox">
-        <table>
-            <thead>
-                <tr>
-                    <th>Användarnamn:</th>
-                    <th>Ta bort:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                userList($db);
-                ?>
-            </tbody>
-        </table>
+            <div class="tablebox">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tävlingsägare:</th>
+                            <th>Länk till tävling:</th>
+                            <th>Ta bort:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        competitionList($db);
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="inputbox">
+                <form method="POST">
+                    <select name="companies" class="selectCompany">
+                        <?php
+                        selectCompany($db);
+                        ?>
+                    </select><br>
+                    <input type="url" id="formUrl" name="formUrl" placeholder="Länk till ny tävling" maxlength="500" autocomplete="off"><br>
+                    <button name="createCompetition" type="submit">Lägg till</button>
+                </form>
+            </div>
         </div>
 
-        <div class="inputbox">
-            <Form method="POST">
-                <input type="text" id="username" name="username" placeholder="Username" autocomplete="off"><br>
-                <input type="password" id="password" name="password" placeholder="Password" autocomplete="off"><br>
-                <button name="createAccount" type="submit">Create Account</button>
-            </Form>
-        </div>
-    </div>
+        <div class="contentbox">
+            <div class="tablebox">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Användarnamn:</th>
+                            <th>Ta bort:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        userList($db);
+                        ?>
+                    </tbody>
+                </table>
+            </div>
 
-    <div class="contentbox">
-        <div class="tablebox">
-        <table>
-            <thead>
-                <tr>
-                    <th>Scannad kod:</th>
-                    <th>När koden scannats:</th>
-                    <th>Scannad av:</th>
-                    <th>Ta bort post:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                //creates sql & creates a list with qr-scan data.
-                $sql = "SELECT qrcodes.qrName, qrscan.dateTime, qrscan.device, qrscan.qrId, qrscan.id AS scanId
+            <div class="inputbox">
+                <Form method="POST">
+                    <input type="text" id="username" name="username" placeholder="Username" autocomplete="off"><br>
+                    <input type="password" id="password" name="password" placeholder="Password" autocomplete="off"><br>
+                    <button name="createAccount" type="submit">Create Account</button>
+                </Form>
+            </div>
+        </div>
+
+        <div class="contentbox">
+            <div class="tablebox">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Scannad kod:</th>
+                            <th>När koden scannats:</th>
+                            <th>Scannad av:</th>
+                            <th>Ta bort post:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //creates sql & creates a list with qr-scan data.
+                        $sql = "SELECT qrcodes.qrName, qrscan.dateTime, qrscan.device, qrscan.qrId, qrscan.id AS scanId
                 FROM qrScan
                 INNER JOIN qrcodes ON qrScan.qrId=qrcodes.id
                 ORDER BY qrscan.device, qrscan.dateTime;";
 
-                $stmt = $db->prepare($sql);
-                $result = $stmt->execute([]);
+                        $stmt = $db->prepare($sql);
+                        $result = $stmt->execute([]);
 
-                while ($row = $stmt->fetch()) {
-                    echo "<tr>
+                        while ($row = $stmt->fetch()) {
+                            echo "<tr>
                     <td>$row[qrName]</td>
                     <td>$row[dateTime]</td>
                     <td>$row[device]</td>
@@ -199,98 +199,98 @@ if (isset($_POST['deleteQrData'])) {
                     <form method='post'><input type='submit' name='deleteQrData[$row[scanId]]' value='Ta bort'></form>
                     </td>
                     </tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
-        
+
             <div class="tablebox">
-        <table>
-            <thead>
-                <tr>
-                    <th>enhet:</th>
-                    <th>antal scanningar:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                //creates sql & creates a list with qr-scan data.
-                $sql = "SELECT `device`, COUNT(*) AS `count` 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>enhet:</th>
+                            <th>antal scanningar:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //creates sql & creates a list with qr-scan data.
+                        $sql = "SELECT `device`, COUNT(*) AS `count` 
                 FROM qrscan
                 GROUP BY `device`;";
 
-                $stmt = $db->prepare($sql);
-                $result = $stmt->execute([]);
+                        $stmt = $db->prepare($sql);
+                        $result = $stmt->execute([]);
 
-                while ($row = $stmt->fetch()) {
-                    echo "<tr>
+                        while ($row = $stmt->fetch()) {
+                            echo "<tr>
                     <td>$row[device]</td>
                     <td>$row[count]</td>
                     </tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
 
             <div class="tablebox">
-        <table>
-            <thead>
-                <tr>
-                    <th>Namn:</th>
-                    <th>antal scanningar:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                //creates sql & creates a list with qr-scan data.
-                $sql = "SELECT qrcodes.qrName, COUNT(*) AS `count`
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Namn:</th>
+                            <th>antal scanningar:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //creates sql & creates a list with qr-scan data.
+                        $sql = "SELECT qrcodes.qrName, COUNT(*) AS `count`
                 FROM qrScan
                 INNER JOIN qrcodes ON qrscan.qrId=qrcodes.id GROUP BY qrId";
 
-                $stmt = $db->prepare($sql);
-                $result = $stmt->execute([]);
+                        $stmt = $db->prepare($sql);
+                        $result = $stmt->execute([]);
 
-                while ($row = $stmt->fetch()) {
-                    echo "<tr>
+                        while ($row = $stmt->fetch()) {
+                            echo "<tr>
                     <td>$row[qrName]</td>
                     <td>$row[count]</td>
                     </tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
 
             <div class="tablebox">
-        <table>
-            <thead>
-                <tr>
-                    <th>Qr-kod:</th>
-                    <th>Länkad sida:</th>
-                    <th>QR koden:</th>
-                    <th>Ta bort qr-kod:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <script>
-                    function deleteQRcode(id) {
-                        return (confirm("Vill du radera Qr-koden och all data som hör till den?"));
-                    }
-                </script>
-                <?php
-                //creates sql & creates a list with the different qr-codes that exist
-                $sql = "SELECT * FROM qrcodes";
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Qr-kod:</th>
+                            <th>Länkad sida:</th>
+                            <th>QR koden:</th>
+                            <th>Ta bort qr-kod:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <script>
+                            function deleteQRcode(id) {
+                                return (confirm("Vill du radera Qr-koden och all data som hör till den?"));
+                            }
+                        </script>
+                        <?php
+                        //creates sql & creates a list with the different qr-codes that exist
+                        $sql = "SELECT * FROM qrcodes";
 
-                $stmt = $db->prepare($sql);
-                $result = $stmt->execute([]);
+                        $stmt = $db->prepare($sql);
+                        $result = $stmt->execute([]);
 
-                while ($row = $stmt->fetch()) {
-                    $filename = "qrcodes/" . $row['qrName'] . ".png";
+                        while ($row = $stmt->fetch()) {
+                            $filename = "qrcodes/" . $row['qrName'] . ".png";
 
 
-                    echo "<tr>
+                            echo "<tr>
                     <td>$row[qrName]</td>
                     <td>$row[Url]</td>
                     <td><a href='$filename' download><img src='$filename' alt='$filename' width='100' height='100'></a></td>
@@ -298,68 +298,65 @@ if (isset($_POST['deleteQrData'])) {
                     <form method='post' onsubmit='return deleteQRcode($row[id])'><input type='submit' name='deleteQr[$row[id]]'  value='Ta bort'></form>
                     </td>
                     </tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
 
-        <div class="inputbox">
-            <form method="POST">
-                <input type="text" id="qrName" name="qrName" placeholder="Namn på qr-kod" autocomplete="off"><br>
-                <input type="url" id="qrUrl" name="qrUrl" placeholder="Länk till qr-kod" autocomplete="off"><br>
-                <button type="submit" name="addQrCode">Lägg till</button>
-            </form>
+            <div class="inputbox">
+                <form method="POST">
+                    <input type="text" id="qrName" name="qrName" placeholder="Namn på qr-kod" autocomplete="off"><br>
+                    <input type="url" id="qrUrl" name="qrUrl" placeholder="Länk till qr-kod" autocomplete="off"><br>
+                    <button type="submit" name="addQrCode">Lägg till</button>
+                </form>
+            </div>
         </div>
-            </div>
 
-            <div class="contentbox">
-        <div class="tablebox">
-        <table>
-            <thead>
-                <tr>
-                    <th>Öppettider:</th>
-                    <th>Datum:</th>
-                    <th>Ta bort:</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                //creates sql & creates a list with the different qr-codes that exist
-                $sqlOpenHours = "SELECT * FROM openhours";
+        <div class="contentbox">
+            <div class="tablebox">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Öppettider:</th>
+                            <th>Datum:</th>
+                            <th>Ta bort:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //creates sql & creates a list with the different qr-codes that exist
+                        $sqlOpenHours = "SELECT * FROM openhours";
 
-                $stmtOpenHours = $db->prepare($sqlOpenHours);
-                $stmtOpenHours->execute([]);
+                        $stmtOpenHours = $db->prepare($sqlOpenHours);
+                        $stmtOpenHours->execute([]);
 
-                while ($row = $stmtOpenHours->fetch()) {
-                    echo "<tr>
+                        while ($row = $stmtOpenHours->fetch()) {
+                            echo "<tr>
                     <td>$row[openHours]</td>
                     <td>$row[openDates]</td>
                     <td>
                         <form method='post'><input type='submit' name='deleteOpenHours[$row[id]]' value='Ta bort'></form>
                     </td>
                 </tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
 
-        <div class="inputbox">
-            <form method="POST">
-                <input type="text" id="openHours" name="openHours" placeholder="Öppettider" autocomplete="off"><br>
-                <input type="text" id="openDates" name="openDates" placeholder="Datum" autocomplete="off"><br>
-                <button type="submit" Name="addOpenHours">Lägg till</button>
-            </form>
-        </div>
+            <div class="inputbox">
+                <form method="POST">
+                    <input type="text" id="openHours" name="openHours" placeholder="Öppettider" autocomplete="off"><br>
+                    <input type="text" id="openDates" name="openDates" placeholder="Datum" autocomplete="off"><br>
+                    <button type="submit" Name="addOpenHours">Lägg till</button>
+                </form>
             </div>
+        </div>
     </main>
 </body>
 
 </html>
 <?php
-
-
-
 unset($_SESSION['alertError']);
 unset($_SESSION['alertSuccess']);
